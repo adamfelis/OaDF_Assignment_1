7f89970f2f66c7befe0397829801d2e17feaa092
Simon Boje Outzen <sbo@csis.dk>
2015-10-04 15:47:52 +0200

    conflict

--- Matlab Solutions/Problem 2/sol_2_0.m
+++ Matlab Solutions/Problem 2/sol_2_0.m
@@ -1,9 +1,6 @@
 % Default starting script for Problem 2
 
-%clear all;
-
-%clear all;
-%close all;
+close all;
 
 f   = @(X)  ( (X(1).^2 + X(2) - 11).^2 + (X(1) + X(2).^2 - 7).^2 ); 
 df  = @(X)  [ (4.*X(1).^3 + 4.*X(1).*X(2) - 42.*X(1) + 2.*X(2).^2 - 14); ...
