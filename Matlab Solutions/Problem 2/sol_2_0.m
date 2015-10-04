b3f0a55d7087da7bfa3bf0f5cb9296223a20460b
Simon Boje Outzen <sbo@csis.dk>
2015-10-04 19:52:23 +0200

    2.4 progress

--- Matlab Solutions/Problem 2/sol_2_0.m
+++ Matlab Solutions/Problem 2/sol_2_0.m
@@ -11,7 +11,9 @@ d2f = @(X)  [ (12.*X(1).^2 + 4.*X(2) -42)   ,   (4.*X(1) + 4.*X(2)) ; ...
           
 options = optimset('Display','off');
 
+
 tolerance_for_SDD_algorithm = 1.0e-10;
+tolerance_for_Newton_algorithm = 1.0e-5;
 tolerance_for_BFGS_algorithm = 1e-05;
 tolerance_for_Levenberg_Marquardt_algorithm = 1e-05;
 
