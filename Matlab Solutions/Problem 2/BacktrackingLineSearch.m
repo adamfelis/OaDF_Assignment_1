9dadc903e5bbe2a349c00a0ed7568ebbb14effa8
Simon Boje Outzen <sboutzen>
2015-10-25 21:49:16 +0100

    small fixes

--- Matlab Solutions/Problem 2/BacktrackingLineSearch.m
+++ Matlab Solutions/Problem 2/BacktrackingLineSearch.m
@@ -1,12 +1,11 @@
 % Backtracking Linesearch
 
 function steplength = BacktrackingLineSearch(xk, direction, steplength, f, df)
-    p_low = 0.25;
-    p_hi = 1 - p_low;
+    p = 0.5;
     c = 0.2;
     
     while(f(xk + steplength * direction) > f(xk) + c * steplength * df(xk)' * direction)
-        steplength = steplength * ((p_low + p_hi) / 2);
+        steplength = steplength * p;
     end
     
    
